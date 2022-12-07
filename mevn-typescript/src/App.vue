ec15714f7fe3f0c98e8b05ee0402c54433cd11c8
DavidVer98 <davidcuevas15@hotmail.com>
2022-12-07 12:31:00 -0300

    Create task components

--- mevn-typescript/src/App.vue
+++ mevn-typescript/src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <NavBar />
+  <main class="py-20">
+    <router-view />
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import NavBar from "./components/NavBar.vue"
+import '../styles/app.css'; // Here we import the CSS file tailwindcss is using
+// import TaskFormCompoment from "./components/TaskForm.vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  // eslint-disable-next-line vue/no-unused-components
+  components: {NavBar},
 });
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
